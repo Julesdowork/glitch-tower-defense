2197d762a2ff7c636f43636d5cc800c705448574
Julian McNeill <julesdowork@gmail.com>
2017-10-23 19:59:12 -0400

    More work on the start of a level

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Camera mainCamera;
+	public Text countdown;
+
+	// Use this for initialization
+	void Start ()
+	{
+		countdown.gameObject.SetActive(false);
+		mainCamera.GetComponent<Animator>().SetTrigger("panTrigger");
+		countdown.gameObject.SetActive(true);
+	}
+}
